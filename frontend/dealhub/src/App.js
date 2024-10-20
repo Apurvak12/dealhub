612b9433ef3428859124defa3f3d3b257db48a72
Gayatri <gayatripande099@gmail.com>
2024-10-20 14:41:38 +0530

    added to App.js

--- frontend/dealhub/src/App.js
+++ frontend/dealhub/src/App.js
@@ -1,20 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import PriceInfoCard from './components/PriceInfoCard';
+import ProductCard from './components/ProductCard';
 
 function App() {
+
+
+  const App = () => {
+    const [products, setProducts] = useState([]);
+    const [priceInfo, setPriceInfo] = useState({ title: '', iconSrc: '', value: '' });
+  
+    useEffect(() => {
+      const fetchProducts = async () => {
+        const response = await axios.get('/api/products'); // Adjust the endpoint as needed
+        setProducts(response.data);
+      };
+  
+      const fetchPriceInfo = async () => {
+        const response = await axios.get('/api/price-info'); // Adjust the endpoint for price info
+        setPriceInfo(response.data);
+      };
+  
+      fetchProducts();
+      fetchPriceInfo();
+    }, []);
+  
   
   return (<>
     <div className="App">
        <Navbar/>
     </div>
+    
     <div>
-    <PriceInfoCard title="Price" iconSrc="/icons/price-icon.png" value="$100" />
+      {/* Render PriceInfoCard */}
+      <PriceInfoCard
+        title={priceInfo.title}
+        iconSrc={priceInfo.iconSrc}
+        value={priceInfo.value}
+      />
 
-
+      {/* Render ProductCards */}
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
     </>
   );
+}
 }
 
 export default App;
