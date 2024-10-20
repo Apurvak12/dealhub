778b156b8723d5f8c7eec50d906d3fa5e3c7b40d
anushka-chimote <chimoteanushka@gmail.com>
2024-10-20 17:16:28 +0530

    Changes made in scrapping

--- backend/scrapping.py
+++ backend/scrapping.py
@@ -107,6 +107,6 @@ def scrape_amazon_product(url):
 
  #Example usage:
 if __name__ == "__main__":
-    url = ""
+    url = "https://www.amazon.com/dp/B07FZ8S74R"
     product_data = scrape_amazon_product(url)
     print(product_data)
